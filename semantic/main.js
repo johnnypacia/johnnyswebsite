54b15beedc86f1d6daafbcf989a624e03f1f3591
Johnny Pacia <johnny.pacia@gmail.com>
2016-02-16 21:38:17 -0500

    adds modal test

--- semantic/main.js
+++ semantic/main.js
@@ -4,5 +4,11 @@ $( function (){
 	$('a.item').click(function(){
 		$('.item').removeClass('active');
 		$(this).addClass('active');
-	})	
+	})
+
+	$('select.dropdown').dropdown('set selected', ['meteor', 'ember']);
+
+	$('.ui.modal')
+  .modal('show')
+;
 });
